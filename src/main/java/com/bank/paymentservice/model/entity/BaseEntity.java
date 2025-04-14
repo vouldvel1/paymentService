6df686599fef56a30109c1a6e6c0548242a83178
Markus <markus.p.developer@inbox.ru>
2025-04-14 22:21:22 +0300

    Refactor BaseEntity class by simplifying imports

--- src/main/java/com/bank/paymentservice/model/entity/BaseEntity.java
+++ src/main/java/com/bank/paymentservice/model/entity/BaseEntity.java
@@ -1,9 +1,6 @@
 package com.bank.paymentservice.model.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -18,6 +15,7 @@ import java.time.LocalDateTime;
 @Setter
 public class BaseEntity {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -29,4 +27,9 @@ public class BaseEntity {
 
     @Version
     private Long version;
+
+    public BaseEntity() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
 }
