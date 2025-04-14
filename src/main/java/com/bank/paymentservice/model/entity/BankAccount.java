bfa3875d95ac4e90bfb47ba499c5402861e3916c
Markus <markus.p.developer@inbox.ru>
2025-04-14 22:24:56 +0300

    Enhance BankAccount class with Lombok annotations and additional fields
    - Add @Getter, @Setter, @NoArgsConstructor, and @AllArgsConstructor annotations for simplified code.
    - Introduce balance, bankAccountId, and currency fields to improve bank account representation.

--- src/main/java/com/bank/paymentservice/model/entity/BankAccount.java
+++ src/main/java/com/bank/paymentservice/model/entity/BankAccount.java
@@ -1,4 +1,22 @@
 package com.bank.paymentservice.model.entity;
 
-public class BankAccount {
+import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Enabled
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankAccount extends BaseEntity {
+
+    private String number;
+    private BigDecimal balance;
+    private Long bankAccountId;
+    private String currency;
 }
