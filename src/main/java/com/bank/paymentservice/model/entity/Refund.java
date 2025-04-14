d9997532bf4c7bd9790a106e3948f64901b792db
Markus <markus.p.developer@inbox.ru>
2025-04-14 23:30:00 +0300

    Add Refund entity with fields and relationships
    Introduce the `Refund` class extending `BaseEntity` with properties for `refundedAmount`, `reason`, and `status`. Implement `ManyToOne` relationship with `PaymentTransaction` and utilize `RefundStatusConverter` for status management.

--- src/main/java/com/bank/paymentservice/model/entity/Refund.java
+++ src/main/java/com/bank/paymentservice/model/entity/Refund.java
@@ -1,4 +1,30 @@
 package com.bank.paymentservice.model.entity;
 
-public class Refund {
+import com.bank.paymentservice.model.enums.RefundStatus;
+import com.bank.paymentservice.model.enums.converter.RefundStatusConverter;
+import jakarta.persistence.Convert;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Refund extends BaseEntity {
+
+    private BigDecimal refundedAmount;
+    private String reason;
+
+    @Convert(converter = RefundStatusConverter.class)
+    private RefundStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "paymentTransactionId", referencedColumnName = "id")
+    private PaymentTransaction paymentTransaction;
 }
